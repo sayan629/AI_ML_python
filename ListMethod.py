f73110e9fe093040c754dc09ca6e96efe330502e
sayan629 <sayanpal771@gmail.com>
2026-01-27 20:14:57 +0530

    Different types of lists method and their uses.

---
+++ ListMethod.py
@@ -0,0 +1,20 @@
+nums=[30,15,50]
+
+nums.append(40) #Add one element at the end.
+print(nums)
+
+
+nums.insert(2,45) #Insert element at the index(middle,first)
+print(nums)
+
+
+nums.sort()
+print(nums) #Sort element in increasing order
+
+
+nums.sort(reverse=True)
+print(nums) #Sort element in decreasing order 
+
+
+nums.reverse()
+print(nums) # To reverse list
