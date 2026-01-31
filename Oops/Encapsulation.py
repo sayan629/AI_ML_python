d022191602929701bec149852445e5392f6cb377
sayan629 <sayanpal771@gmail.com>
2026-01-31 20:56:33 +0530

    Concept od Data hiding in python

--- Oops/Encapsulation.py
+++ Oops/Encapsulation.py
@@ -6,8 +6,12 @@ class BankAccount:
         
     def get_deposit(self):  #for accessing private data we use getter and setter method
         return self.__deposit
+    
+    def set_deposit(self,new_deposit): #setter 
+        self.__deposit=new_deposit
 
 acc1=BankAccount("Sayan Pal", 100_000,25_000)
+acc1.set_deposit(20_000)
 
 print(acc1.name, acc1._balance,acc1.get_deposit()) 
     
