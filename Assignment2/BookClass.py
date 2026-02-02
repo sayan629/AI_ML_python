e6ecfc555a9ddf56ad2ce78b22b801fa044d255a
sayan629 <sayanpal771@gmail.com>
2026-02-02 22:39:52 +0530

    Implement Book class with add, display, and count reviews functionality

---
+++ Assignment2/BookClass.py
@@ -0,0 +1,22 @@
+class Book:
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author
+        self.reviews =[]
+    
+    def add_reviwes(self,review):
+        self.reviews.append(review)
+    
+    def count_reviews(self):
+        return len(self.reviews)
+
+    def display_reviews(self):
+        for r in self.reviews:
+            print(r)
+
+b=Book("Python","Guido")
+b.add_reviwes("Nice Book")
+b.add_reviwes("Very Helpful")
+b.add_reviwes("This book used for beginners")
+b.display_reviews()
+print("Total Reviews:", b.count_reviews())
