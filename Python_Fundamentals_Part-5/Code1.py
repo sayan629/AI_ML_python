116612b1010c750e79ef4c4e55578723273ac0a1
sayan629 <sayanpal771@gmail.com>
2026-02-04 20:06:15 +0530

    Check how write operation is performed which is stored in Sample1.txt

---
+++ Python_Fundamentals_Part-5/Code1.py
@@ -0,0 +1,8 @@
+f=open("D:\PYTHON_CODE\Python_Fundamentals_Part-5\Sample1.txt","w")
+
+#write function override the previous data in the txt file which you provide
+## and rewrite data in the same txt file
+
+f.write("Text to overwrite /n the complete data")
+
+f.close()
